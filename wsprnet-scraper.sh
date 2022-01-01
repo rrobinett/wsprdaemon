fbcead55c2eb1784ba2cc6b3c77eb9546981936c
Rob <rob@robinett.us>
2022-01-01 14:44:34 +0000

    On WD1: scraper logs late-delivered spots

--- wsprnet-scraper.sh
+++ wsprnet-scraper.sh
@@ -225,7 +225,12 @@ function wsprnet_to_csv() {
         fi
         grep    "^20" ${WSPRNET_SCRAPER_TMP_PATH}/filtered_spots.csv > ${WSPRNET_SCRAPER_TMP_PATH}/fixed_spots.csv
         if [[ -s ${WSPRNET_SCRAPER_TMP_PATH}/fixed_spots.csv ]]; then
-            wd_logger 1 "$(printf "adding %4d spots at epoch %d == '%(%Y-%m-%d:%H:%M)T'"  $(wc -l < ${WSPRNET_SCRAPER_TMP_PATH}/fixed_spots.csv)  ${spot_epoch} ${spot_epoch})"
+            local spots_to_add_count=$(wc -l < ${WSPRNET_SCRAPER_TMP_PATH}/fixed_spots.csv)
+            wd_logger 1 "$(printf "adding %4d spots at epoch %d == '%(%Y-%m-%d:%H:%M)T'"  ${spots_to_add_count}  ${spot_epoch} ${spot_epoch})"
+            local this_epcoch_age_minutes=$(( (${epochs_list[-1]} - ${spot_epoch}) / 60 ))
+            if [[ ${this_epcoch_age_minutes} -gt 30 ]]; then
+                wd_logger 1 "Found old spots:\n$(head -n 4 ${WSPRNET_SCRAPER_TMP_PATH}/fixed_spots.csv)"
+            fi
             cat ${WSPRNET_SCRAPER_TMP_PATH}/fixed_spots.csv  >> ${wsprnet_csv_spot_file}
         fi
     done
