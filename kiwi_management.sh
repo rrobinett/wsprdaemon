4d483ebc08053b0958bcfdeda59b03702f8916df
Erwin (de F/PE3ES) <waterwin2@yahoo.com>
2022-01-24 18:30:32 +0100

    Update kiwi_management.sh
    Typo edit

--- kiwi_management.sh
+++ kiwi_management.sh
@@ -10,13 +10,13 @@ function check_kiwi_wspr_channels() {
         return
     fi
     if [[ ${users_max} -lt 8 ]]; then
-        wd_logger 3 " Kiwi '${kiwi_name}' is configured for ${users_max} users, not in 8 channel mode.  So nothing to do"
+        wd_logger 3 " Kiwi '${kiwi_name}' is configured for ${users_max} users, not in 8 channel mode. So nothing to do"
         return
     fi
 
     local active_receivers_list=$( curl -s --connect-timeout 5 ${kiwi_ip}/users | sed -n '/"i":\([0-9]\),"n"/s//\n\1/gp' | ${GREP_CMD} "^[0-9]" )
     if [[ -z "${active_receivers_list}" ]];  then
-        wd_logger 2 " Kiwi '${kiwi_name}' not reporting users status or there are no active rx channels on it.  So nothing to do"
+        wd_logger 2 " Kiwi '${kiwi_name}' not reporting users status or there are no active rx channels on it. So nothing to do"
         return
     fi
     local wd_arg=$(echo "Kiwi ${kiwi_name} has active listeners:\n${active_receivers_list}")
@@ -30,7 +30,7 @@ function check_kiwi_wspr_channels() {
     local wd_ch_01_listeners_count=$( ${GREP_CMD} "^[01]:.wsprdaemon" <<< "${active_receivers_list}" | wc -l) 
     wd_logger 3 "Kiwi '${kiwi_name}' has ${wd_listeners_count} WD listeners of which ${wd_ch_01_listeners_count} listeners are on ch 0 or ch 1"
     if [[ ${wd_listeners_count} -le 6 && ${wd_ch_01_listeners_count} -gt 0 ]]; then
-        wd_logger 1 "WARNING, Kiwi '${kiwi_name}' configured in 8 channel mode has ${wd_listeners_count} WD listeners.So all of them should be on rx ch 2-7,  but  ${wd_ch_01_listeners_count} listeners are on ch 0 or ch 1: \n${active_receivers_list}"
+        wd_logger 1 "WARNING, Kiwi '${kiwi_name}' configured in 8 channel mode has ${wd_listeners_count} WD listeners. So all of them should be on rx ch 2-7, but  ${wd_ch_01_listeners_count} listeners are on ch 0 or ch 1: \n${active_receivers_list}"
         if ${GREP_CMD} -q ${kiwi_name} <<< "${RUNNING_JOBS[@]}"; then
             wd_logger 1 " found '${kiwi_name}' is in use by this instance of WD, so add code to clean up the RX channels used"
             ### TODO: recover from listener on rx 0/1 code here 
@@ -49,7 +49,7 @@ function check_kiwi_rx_channels() {
     wd_logger 2 " starting a check of rx channel usage on all Kiwis"
 
     for kiwi in ${kiwi_list} ; do
-        wd_logger 4 " check active users on KIWI '${kiwi}'"
+        wd_logger 4 " check active users on Kiwi '${kiwi}'"
         check_kiwi_wspr_channels ${kiwi}
     done
 }
@@ -57,7 +57,7 @@ function check_kiwi_rx_channels() {
 ### If there are no GPS locks and it has been 24 hours since the last attempt to let the Kiwi get lock, stop all jobs for X seconds
 declare KIWI_GPS_LOCK_CHECK=${KIWI_GPS_LOCK_CHECK-yes} ## :=no}
 declare KIWI_GPS_LOCK_CHECK_INTERVAL=600 #$((24 * 60 * 60))  ### Seconds between checks
-declare KIWI_GPS_STARUP_LOCK_WAIT_SECS=60               ### Wher first starting and the Kiwi reports no GPS lock, poll for lock this many seconds
+declare KIWI_GPS_STARUP_LOCK_WAIT_SECS=60                    ### When first starting and the Kiwi reports no GPS lock, poll for lock this many seconds
 declare KIWI_GPS_LOCK_LOG_DIR=${WSPRDAEMON_TMP_DIR}/kiwi_gps_status
 
 function check_kiwi_gps() {
@@ -65,10 +65,10 @@ function check_kiwi_gps() {
 
     local kiwi
     local kiwi_list=$(list_kiwis)
-    wd_logger 4 " got list of all defined KIWIs = '${kiwi_list}'"
+    wd_logger 4 " got list of all defined Kiwis = '${kiwi_list}'"
 
     for kiwi in ${kiwi_list} ; do
-        wd_logger 4 " check lock on KIWI '${kiwi}'"
+        wd_logger 4 " check lock on Kiwi '${kiwi}'"
         let_kiwi_get_gps_lock ${kiwi}
     done
     wd_logger 2 " check completed"
@@ -83,20 +83,20 @@ function let_kiwi_get_gps_lock() {
     ### Check to see if Kiwi reports gps status and if the Kiwi is locked to enough satellites
     local kiwi_status=$(curl -s --connect-timeout 5 ${kiwi_ip}/status)
     if [[ -z "${kiwi_status}" ]]; then
-        wd_logger 1 " got no response from kiwi '${kiwi_name}'"
+        wd_logger 1 " got no response from Kiwi '${kiwi_name}'"
         return
     fi
     local kiwi_gps_good_count=$(awk -F = '/gps_good=/{print $2}' <<< "${kiwi_status}" )
     if [[ -z "${kiwi_gps_good_count}" ]]; then
-        wd_logger 1 "kiwi '${kiwi_name}' is running SW which doesn't report gps_good status"
+        wd_logger 1 " kiwi '${kiwi_name}' is running SW which doesn't report gps_good status"
         return
     fi
     declare GPS_MIN_GOOD_COUNT=4
     if [[ ${kiwi_gps_good_count} -lt ${GPS_MIN_GOOD_COUNT} ]]; then
-        wd_logger 2 "kiwi '${kiwi_name}' reports '${kiwi_gps_good_count}' good GPS which is less than the min of ${GPS_MIN_GOOD_COUNT} we require.  So GPS is bad on this Kiwi"
+        wd_logger 2 " kiwi '${kiwi_name}' reports '${kiwi_gps_good_count}' good GPS which is less than the min of ${GPS_MIN_GOOD_COUNT} we require. So GPS is bad on this Kiwi"
         ### TODO: don't perturb the Kiwi too often if it doesn't have GPS lock
     else
-        wd_logger 3 "kiwi '${kiwi_name}' reports '${kiwi_gps_good_count}' good GPS which is greater than or equal to the min of ${GPS_MIN_GOOD_COUNT} we require.  So GPS is OK on this Kiwi"
+        wd_logger 3 " kiwi '${kiwi_name}' reports '${kiwi_gps_good_count}' good GPS which is greater than or equal to the min of ${GPS_MIN_GOOD_COUNT} we require. So GPS is OK on this Kiwi"
         ### TODO:  just return here once I am confident that further checks are not needed
         ### return
     fi
@@ -118,35 +118,35 @@ function let_kiwi_get_gps_lock() {
     local seconds_since_last_check=$(( ${current_time} - ${kiwi_last_gps_check_time} ))
 
     if [[ ${kiwi_last_fixes_count} -gt 0 ]] && [[ ${seconds_since_last_check} -lt ${KIWI_GPS_LOCK_CHECK_INTERVAL} ]]; then
-        wd_logger 3 "too soon to check KIWI '${kiwi_name}'.  Only ${seconds_since_last_check} seconds since last check"
+        wd_logger 3 " too soon to check Kiwi '${kiwi_name}'. Only ${seconds_since_last_check} seconds since last check"
         return
     fi
     ### fixes is 0 OR it is time to check again
     local kiwi_fixes_count=$(awk -F = '/fixes=/{print $2}' <<< "${kiwi_status}" )
     if [[ -z "${kiwi_fixes_count}" ]]; then
-        wd_logger 1 "kiwi '${kiwi_name}' is running SW which doesn't report fixes status"
+        wd_logger 1 " kiwi '${kiwi_name}' is running SW which doesn't report fixes status"
         return
     fi
-    wd_logger 3 "got new fixes count '${kiwi_fixes_count}' from kiwi '${kiwi_name}'"
+    wd_logger 3 " got new fixes count '${kiwi_fixes_count}' from kiwi '${kiwi_name}'"
     if [[ ${kiwi_fixes_count} -gt ${kiwi_last_fixes_count} ]]; then
-        wd_logger 3 "Kiwi '${kiwi_name}' is locked since new count ${kiwi_fixes_count} is larger than old count ${kiwi_last_fixes_count}"
+        wd_logger 3 " Kiwi '${kiwi_name}' is locked since new count ${kiwi_fixes_count} is larger than old count ${kiwi_last_fixes_count}"
         echo ${kiwi_fixes_count} > ${kiwi_gps_log_file}
         return
     fi
     if [[ ${kiwi_fixes_count} -lt ${kiwi_last_fixes_count} ]]; then
-        wd_logger 2 "Kiwi '${kiwi_name}' is locked but new count ${kiwi_fixes_count} is less than old count ${kiwi_last_fixes_count}. Our old count may be stale (from a previous run), so save this new count"
+        wd_logger 2 " Kiwi '${kiwi_name}' is locked but new count ${kiwi_fixes_count} is less than old count ${kiwi_last_fixes_count}. Our old count may be stale (from a previous run), so save this new count"
         echo ${kiwi_fixes_count} > ${kiwi_gps_log_file}
         return
     fi
-    wd_logger 2 "Kiwi '${kiwi_name}' reporting ${GPS_MIN_GOOD_COUNT} locks, but new count ${kiwi_fixes_count} == old count ${kiwi_last_fixes_count}, so fixes count has not changed"
-    ### GPS fixes count has not changed.  If there are active users or WD clients, kill those sessions so as to free the Kiwi to search for sats
+    wd_logger 2 " Kiwi '${kiwi_name}' reporting ${GPS_MIN_GOOD_COUNT} locks, but new count ${kiwi_fixes_count} == old count ${kiwi_last_fixes_count}, so fixes count has not changed"
+    ### GPS fixes count has not changed. If there are active users or WD clients, kill those sessions so as to free the Kiwi to search for sats
     local active_receivers_list=$( curl -s --connect-timeout 5 ${kiwi_ip}/users | sed -n '/"i":\([0-9]\),"n"/s//\n\1/gp' | ${GREP_CMD} "^[0-9]" )
     if [[ -z "${active_receivers_list}" ]];  then
-        wd_logger 2 "found no active rx channels on Kiwi '${kiwi_name}, so it is already searching for GPS"
+        wd_logger 2 " found no active rx channels on Kiwi '${kiwi_name}, so it is already searching for GPS"
         touch ${kiwi_gps_log_file}
         return
     fi
-    wd_logger 2 "This is supposed to no longer be needed, but it appears that we terminate active users on Kiwi '${kiwi_name}' so it can get GPS lock: \n${active_receivers_list}"
+    wd_logger 2 " This is supposed to no longer be needed, but it appears that we terminate active users on Kiwi '${kiwi_name}' so it can get GPS lock: \n${active_receivers_list}"
 }
 
 
