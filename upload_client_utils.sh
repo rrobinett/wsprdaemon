742c96dad1ead6bc852ee549e3d6448edfede3b4
Rob Robinett <rob@robinett.us>
2022-02-06 12:40:55 -1000

    In Maui: sort wsprdaemon upload files by freq

--- upload_client_utils.sh
+++ upload_client_utils.sh
@@ -405,7 +405,7 @@ function upload_to_wsprdaemon_daemon() {
         if [[ ${ret_code} -ne 0 ]]; then
             wd_logger 1 "ERROR: 'tar cfj ${tar_file_path} \${source_file_list[@]}' => ret_code ${ret_code}"
         else
-            wd_logger 1 "Starting curl upload of '${tar_file_path}' of size $( ${GET_FILE_SIZE_CMD} ${tar_file_path} ) which contains $(cat ${source_file_list[@]} | wc -c)  bytes from ${#source_file_list[@]} spot and noise files. Spots are::\n$(sort -k5,5n ${spot_file_list[*]})"
+            wd_logger 1 "Starting curl upload of '${tar_file_path}' of size $( ${GET_FILE_SIZE_CMD} ${tar_file_path} ) which contains $(cat ${source_file_list[@]} | wc -c)  bytes from ${#source_file_list[@]} spot and noise files. Spots are::\n$(sort -k6,6n ${spot_file_list[*]})"
             local upload_user=${SIGNAL_LEVEL_FTP_LOGIN-noisegraphs}
             local upload_password=${SIGNAL_LEVEL_FTP_PASSWORD-xahFie6g}    ## Hopefully this default password never needs to change
             local upload_url=${SIGNAL_LEVEL_FTP_URL-graphs.wsprdaemon.org/upload}/${tar_file_name}
