016467f63d491c4792275827105c0e11bc067abe
Rob Robinett <rob@robinett.us>
2024-01-31 08:00:27 -0800

    Add Franco's code which converts wav files to DRF format and uploads them.  Uploading is disabled

--- grape-utils.sh
+++ grape-utils.sh
@@ -27,6 +27,8 @@ declare -r MINUTES_LIST=( $(seq -f "%02g" 0 59) )
 declare -r GRAPE_24_HOUR_10_HZ_WAV_FILE_NAME="24_hour_10sps_iq.wav"
 declare -r GRAPE_24_HOUR_10_HZ_WAV_STATS_FILE_NAME="24_hour_10sps_iq.stats"
 export     RSYNC_PASSWORD="${GRAPE_PSWS_PASSWORD-}"          ### This is the 'token' issued by the PSWS server which we use as the rsync password
+declare -r PSWS_URL="pswsnetwork.caps.ua.edu"
+
 
 ### Return codes can only be in the range 0-255.  So we reserve a few of those codes for the following routines to commmunicate errors back to grape calling functions
 declare -r GRAPE_ERROR_RETURN_BASE=240
@@ -64,7 +66,85 @@ function grape_init() {
     return 0
 }
 
-declare -r PSWS_URL="pswsnetwork.caps.ua.edu"
+# upload_24hour_wavs_to_grape_drf_server() is derived from Franco's grape.sh
+# go from wav files to Grape:
+# - convert from wav to Digital RF dataset
+# - upload Digital RF dataset directory to PSWS network
+#
+# Franco Venturi - K4VZ - Mon 22 Jan 2024 01:20:14 PM UTC
+# Rob Robinett - AI6VN - Mon Jan 30 2024 modified to be part of a Wsprdaemon client
+
+set -euo pipefail
+
+declare -r PSWS_SERVER_URL='pswsnetwork.caps.ua.edu'
+declare -r UPLOAD_TO_PSWS_SERVER_COMPLETED_FILE_NAME='pswsnetwork_upload_completed'
+declare -r WAV2GRAPE_PYTHON_CMD="${WSPRDAEMON_ROOT_DIR}/wav2grape.py"
+
+
+### Given: the path to the .../wav-archive.d/<DATE>/<RPORTER>_<GRID> directory under which there  may be  one or  more receivers with 24 hour wav files which have not 
+###  been converted to DRF and uploaded to the GRAPE server
+### Returns:  0 on nothing to do or success on uploading
+
+declare    WD_TEST_RX_DIR=~/wsprdaemon/wav-archive.d/20240128/KFS=Q_CM87tj
+
+function upload_24hour_wavs_to_grape_drf_server() {
+    local reporter_wav_root_dir=$( realpath $1 )
+
+    if [[ ! -d ${reporter_wav_root_dir} ]]; then
+        wd_logger 1 "ERROR:  reporter_wav_root_dir='${reporter_wav_root_dir}' does not exist"
+    fi
+    wd_logger 1 "Upload the wav files in ${reporter_wav_root_dir} to the GRAPE server"
+
+    local reporter_upload_complete_file_name="${reporter_wav_root_dir}/${UPLOAD_TO_PSWS_SERVER_COMPLETED_FILE_NAME}"
+
+    if [[ -f ${reporter_upload_complete_file_name} ]]; then
+        wd_logger 1  "File ${reporter_upload_complete_file_name} exists, so upload of wav files has already been successful"
+        return 0
+    fi
+    ### On the WD client the flac and 24hour.wav files are cached in the non-volitile  file system which has the format:
+    ### ...../wsprdaemon/wav-archive.d/<DATE>/<WSPR_REPORTER_ID>_<WSPR_REPORTER_GRID>/<WD_RECEIVER_NAME>@<PSWS_SITE_ID>_<PSWS_INSTRUMENT_NUMBER>/<BAND>
+    ### WSPR_REPORTER_ID, WSPR_REPORTER_GRID and WD_RECEIVER and WD_RECEIVER_NAME are assigned by the WD client and entered into the wsprdaemon.conf file
+    ### Each WD client can support multiple WSPR_REPORTER_IDs, each of which can have the same or a unique WSPR_REPORTER_GRID
+    ### Each WSPR_REPORTER_ID+WSPR_REPORTER_GRID is associated with one or more WSPR_RECIEVER_NAMEs, and each of those will support one or more BANDS
+    ###
+    local dir_path_list=( ${reporter_wav_root_dir//\// } )
+    local wav_date=${dir_path_list[-2]}
+    local reporter_info=${dir_path_list[-1]}
+    local reporter_id=${reporter_info%_*}         ### Chop off the _GRID to get the WSPR reporter id
+    local reporter_grid=${reporter_info#*_}       ### Chop off the REPROTER_ID to get the grid
+
+    ### Search each receiver for wav files
+    local receiver_dir
+    local receiver_dir_list=( $(find "${reporter_wav_root_dir}" -mindepth 1 -maxdepth 1 -type d | sort ) )
+    if [[ ${#receiver_dir_list[@]} -eq 0 ]]; then
+        wd_logger 1  "There are no receiver dirs under ${reporter_wav_root_dir}"
+        return 1
+    fi
+    # set -x
+    for receiver_dir in ${receiver_dir_list[@]} ; do
+        local receiver_info="${receiver_dir##*/}"
+        local receiver_name="${receiver_info%@*}"
+        local pswsnetwork_info="${receiver_info#*@}"
+        local psws_station_id="${pswsnetwork_info%_*}"
+        local psws_instrument_id="${pswsnetwork_info#*_}"
+        wd_logger 1  "Processing ${receiver_dir}:
+        date: ${wav_date}- site: ${reporter_id} - receiver_name: $receiver_name - psws_station_id: $psws_station_id - psws_instrument_id: $psws_instrument_id" 1>&2
+        rm -rf  ${GRAPE_TMP_DIR}/*
+        local receiver_tmp_dir="$("$WAV2GRAPE_PYTHON_CMD" -i "$receiver_dir" -o "$GRAPE_TMP_DIR" 2> ${WAV2GRAPE_PYTHON_CMD%.py}.log)"
+        wd_logger 1  "DRF files can be found in ${receiver_tmp_dir}.  Now upload them.."
+        # upload to PSWS network
+        (
+        cd "$(dirname "$receiver_tmp_dir")"
+        {
+            echo "put -r .";
+            echo "mkdir c$(basename "$receiver_tmp_dir")\#$psws_instrument_id\#$(date +%Y-%m%dT%H-%M)";
+        } > /dev/null # | echo  sftp -b - "$psws_station_id"@"$PSWS_SERVER_URL"
+        )
+    done
+    touch "${reporter_upload_complete_file_name}"
+    wd_logger 1  "Indicate that uploads have been successful by creating '${reporter_upload_complete_file_name}'"
+}
+
 declare -r WD_PSWS_PUB_FILE="${WSPRDAEMON_ROOT_DIR}/grape_pswsnetwork.pub"
 
 function grape_test_ssh_auto_login() {
@@ -503,6 +583,9 @@ function grape_menu() {
             ;; 
         -r) 
             grape_repair_all_dates_flacs
+            ;;
+        -u)
+            upload_24hour_wavs_to_grape_drf_server ${WD_TEST_RX_DIR}
             ;;
         -U)
             grape_upload_all_10hz_wavs
