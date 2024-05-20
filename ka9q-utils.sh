fcd1576e048a9d1712e746f3ef2e39afa4eb6fcb
Rob <rob@robinett.us>
2024-05-20 03:44:45 +0000

    Configure KA9Q from May 7

--- ka9q-utils.sh
+++ ka9q-utils.sh
@@ -37,7 +37,7 @@ declare KA9Q_RADIO_NWSIDOM="${KA9Q_RADIO_ROOT_DIR}/nwisdom"     ### This is crea
 declare FFTW_DIR="/etc/fftw"                                    ### This is the directory where radiod looks for a wisdomf
 declare FFTW_WISDOMF="${FFTW_DIR}/wisdomf"                      ### This the wisdom file it looks for
 
-declare KA9Q_REQUIRED_COMMIT_SHA="${KA8Q_REQUIRED_COMMIT_SHA-ac86efc30a3610a153eaadd354351155d3f8ba18}"   ### Default to 4/22/24 with many Phil enhancements
+declare KA9Q_REQUIRED_COMMIT_SHA="${KA8Q_REQUIRED_COMMIT_SHA-307030b45f63fc735420431efb662523360f4a2c}"   ### Default to  Tue May 7 00:49:25 2024 -070
 declare GIT_LOG_OUTPUT_FILE="${WSPRDAEMON_TMP_DIR}/git_log.txt"
 
 function get_current_commit_sha() {
