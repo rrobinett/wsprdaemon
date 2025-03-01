a5273ead0344b0e066a69def55fc90d135f49b81
Rob Robinett <rob@robinett.us>
2025-03-01 20:19:04 +0000

    fix bad call wd_loggeer()

--- decoding.sh
+++ decoding.sh
@@ -804,7 +804,7 @@ function file_is_closed_or_last_write_was_seconds_ago() {
                 local max_delay_secs=${RECORDING_MAX_DELAY_SECONDS-2}  ### If 'stat' reports last write is greater than 2 seconds 
                 local min_early_secs=${RECORDING_MIN_EARLY_SECONDS-59} ### and it is earlier than second 59, then log error
                 if (( (seconds > max_delay_secs) && (seconds < min_early_secs) )); then
-                    wd_loggeer 1 "ERROR: File ${newest_file_name##*/} last write time ${date_time_list[*]} is not in second 00"
+                    wd_logger 1 "ERROR: File ${newest_file_name##*/} last write time ${date_time_list[*]} is not in second 00"
                 else
                     wd_logger 1 "File ${newest_file_name##*/} last write time ${date_time_list[*]} shows it has been closed in second $seconds after a wait of $(( SECONDS - start_second )) seconds"
                 fi
