d2091b1adb5741476e6b671407212e0e68471d4c
Rob Robinett <rob@robinett.us>
2025-11-12 02:06:28 +0000

    Remove server-specific files - moved to wsprdaemon-server repo
    All server services have been moved to:
    https://github.com/rrobinett/wsprdaemon-server
    
    This includes:
    - wsprnet_scraper (scrapes wsprnet.org)
    - wsprdaemon_server (processes uploaded .tbz files)
    - wsprdaemon_reflector (distributes files to multiple servers)
    - All server installation and maintenance scripts
    - Server-specific documentation
    
    This repository now contains only client-side code:
    - Receiver management (Kiwi, KA9Q)
    - Recording, decoding, posting
    - Client configuration and utilities

--- wsprnet_scraper_setup.sh
+++
@@ -1,9 +0,0 @@
-# Create a virtual environment in your project directory
-cd ~/wsprdaemon
-python3 -m venv venv
-
-# Activate the virtual environment
-source venv/bin/activate
-
-# Now install the packages
-pip3 install requests clickhouse-connect numpy
