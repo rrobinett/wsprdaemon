ad3bda3193f11e858cd315f7665396e0a083cc6b
Rob Robinett <rob@robinett.us>
2026-01-10 10:27:23 -0800

    Specify new qweb which supports 8 bit spectrum => 128 Kbps on LAN

--- ka9q-utils.sh
+++ ka9q-utils.sh
@@ -809,9 +809,9 @@ function ka9q_web_service_daemon() {
 
         # Conditionally add -n "${server_description}" if KA9Q_WEB_TITLE is defined
         if [[ -n "${server_description}" ]]; then
-            ${KA9Q_WEB_CMD} ${WF_BIT_DEPTH_ARG--b2} -m ${status_dns_name} -p ${server_ip_port} -n "${server_description}" >& ${daemon_log_file}
+            ${KA9Q_WEB_CMD} ${WF_BIT_DEPTH_ARG--b1} -m ${status_dns_name} -p ${server_ip_port} -n "${server_description}" >& ${daemon_log_file}
         else
-            ${KA9Q_WEB_CMD} ${WF_BIT_DEPTH_ARG--b2} -m ${status_dns_name} -p ${server_ip_port} >& ${daemon_log_file}
+            ${KA9Q_WEB_CMD} ${WF_BIT_DEPTH_ARG--b1} -m ${status_dns_name} -p ${server_ip_port} >& ${daemon_log_file}
         fi
 
         rc=$? ; if (( rc )); then
@@ -1861,7 +1861,7 @@ declare GITHUB_PROJECTS_LIST=(
     "ft8_lib                            ${KA9Q_FT8_COMMIT_CHECK-yes}     ${KA9Q_FT8_ENABLED-yes}     build_ka9q_ft8      NONE                            ${KA9Q_FT8_GIT_URL-https://github.com/ka9q/ft8_lib.git}                    ${KA9Q_FT8_COMMIT-6069815dcccac8f8446b0d55f5a27d6fb388cb70}"
     "ftlib-pskreporter                  ${PSK_UPLOADER_COMMIT_CHECK-yes} ${PSK_UPLOADER_ENABLED-yes} build_psk_uploader  NONE                            ${PSK_UPLOADER_GIT_URL-https://github.com/pjsg/ftlib-pskreporter.git}  ${PSK_UPLOADER_COMMIT-a612dce854f548133907f3c486f90db587515de6}"
     "onion                              ${ONION_COMMIT_CHECK-yes}        ${ONION_ENABLED-yes}        build_onion         ${ONION_LIBS_NEEDED// /,}       ${ONION_GIT_URL-https://github.com/davidmoreno/onion}                         ${ONION_COMMIT-de8ea938342b36c28024fd8393ebc27b8442a161}"
-    "${KA9Q_WEB_PROJECT_NAME-ka9q-web}  ${KA9Q_WEB_COMMIT_CHECK-yes}     ${KA9Q_WEB_ENABLED-yes}     build_ka9q_web      NONE                            ${KA9Q_WEB_GIT_URL-https://github.com/wa2n-code/ka9q-web}                  ${KA9Q_WEB_COMMIT-c394949305685fdbc0d1df77c1e8ecec4200c71a}"
+    "${KA9Q_WEB_PROJECT_NAME-ka9q-web}  ${KA9Q_WEB_COMMIT_CHECK-yes}     ${KA9Q_WEB_ENABLED-yes}     build_ka9q_web      NONE                            ${KA9Q_WEB_GIT_URL-https://github.com/wa2n-code/ka9q-web}                  ${KA9Q_WEB_COMMIT-9e798bd9050c03683f0912ae97cecd3bbd788464}"
 )
 ###
 function ka9q-services-setup() {
